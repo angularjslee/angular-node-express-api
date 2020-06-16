8b71cbc903b234bc7bb695cfe85c5c13f65d538e
longl <longl@rocketsoftware.com>
2020-06-16 11:40:17 +0800

    Creating API endpoint in Node.js & Express

---
+++ models/model.customer.ts
@@ -0,0 +1,17 @@
+/**
+ * Created by longl on 6/15/2020.
+ */
+class CustomerModel
+{
+    constructor(uid, first_name, last_name, email, zipcode, password)
+    {
+        this.uid = uid;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.zipcode = zipcode;
+        this.password = password;
+    }
+}
+
+module.exports = CustomerModel;
